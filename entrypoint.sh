6f9899ade283aece198d87e4470e1d81d6f1b6e1
patrick0057 <44879867+patrick0057@users.noreply.github.com>
2019-05-16 19:58:59 -0600

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PENDPOINTIP=$(env | grep \/kubernetes | grep key | sed -e 's/.*etcd-\(.*\)-key.pem/\1/g' | sed -e 's/-/\./g')
+sed -i 's,cert_file: .*,cert_file: '${ETCDCTL_CERT%$'\r'}',g' "/tmp/test-etcd.yaml"
+sed -i 's,key_file: .*,key_file: '${ETCDCTL_KEY%$'\r'}',g' "/tmp/test-etcd.yaml"
+sed -i "s,- targets: .*,- targets: \[\'${PENDPOINTIP%$'\r'}:2379\'\],g"  "/tmp/test-etcd.yaml"
+service grafana-server start
+nohup /tmp/prometheus     -config.file /tmp/test-etcd.yaml     -web.listen-address ":9090"     -storage.local.path "test-etcd.data" >> /tmp/test-etcd.log  2>&1
